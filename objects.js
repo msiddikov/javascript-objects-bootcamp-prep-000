bbe8783055fbb98c86c50dd147dd935f23c7facc
Mukhammadayub Siddikov <m.siddikov@gmail.com>
2017-09-13 05:20:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,5 +11,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 
 function removeFromPlaylist(playlist, artistName){
 
-  return delete playlist.artistName
+  delete playlist.artistName;
+  return playlist
 }
