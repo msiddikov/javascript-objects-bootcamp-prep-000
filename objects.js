be5cd5a1659f433e656c394393369f6ebba566c4
Mukhammadayub Siddikov <m.siddikov@gmail.com>
2017-09-13 05:18:03 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  GreenDay: "21 Guns",
+  SnowPatrol: "Run",
+  Imaginedragons: "Dream"
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+  console.log(playlist);
+  return playlist
+}
